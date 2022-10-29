4eff3dcb06212fdf24b6c53a7ce697a5ce3688f2
alexmedinasf <alexsales001@hotmail.com>
2022-10-28 22:48:59 -0300

    fixes

--- src/modules/reservationEventListener.js
+++ src/modules/reservationEventListener.js
@@ -55,14 +55,12 @@ static reserveBtn = () => {
     const parent = e.target.parentElement.parentElement.parentElement;
     const src = parent.children[0].getAttribute('src');
     const pickName = parent.children[1].children[0].textContent;
-    const underSpace = document.querySelector('.under-space');
     const appendIt = document.querySelector('.image-body');
     const starNumber = parent.children[1].children[1].children[1].textContent;
     const reservationBody = document.querySelector('.section-reservations');
     if (!tar.classList.contains('reservationsBtn')) return null;
 
     appendIt.innerHTML = characterImg;
-    underSpace.innerHTML = underSpaceContent;
     reservationBody.classList.add('active');
   });
 }
